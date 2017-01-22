cfb5ddfa0c7ee2542504cbbcdc3fcd4105596c19
Tom Budde Dalsgaard <Tom Budde Dalsgaard>
2017-01-22 18:10:00 +0100

    Some styling li

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,7 +1,15 @@
-var calcInput = [
+var calcInput = "";
+
+var calcInputArray = [
     {
         "id": 1,
         "inputText": "0"
     }
 ];
 
+var calcInputField = document.getElementById("calcOutput");
+
+function calcClick(btn){
+    calcInput+="1";
+    calcInputField.innerHTML = calcInput;
+}
