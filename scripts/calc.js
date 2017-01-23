14ccd8e56efdbab74bc949b411185025dd403c50
Admin <Admin>
2017-01-23 13:37:51 +0100

    Number Buttons fixed

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,4 +1,6 @@
-var calcInput = "";
+
+var calcInput = "0";
+var prevcalcInput = "0";
 
 var calcInputArray = [
     {
@@ -9,7 +11,37 @@ var calcInputArray = [
 
 var calcInputField = document.getElementById("calcOutput");
 
+//calcInputField.preventDefault();
+
 function calcClick(btn){
-    calcInput+="1";
-    calcInputField.value = calcInput;
+    //btn.preventDefault();
+    //calcInputField.preventDefault();
+    try{
+    
+    switch(btn) {
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+        case "0":
+            calcInput+=btn;
+            break;
+        default:
+            break;
+        }
+    }
+    catch(err) {
+        document.getElementById("demo").innerHTML = err.message;
+    }
+
+    if (isNaN(parseFloat(calcInput)))
+        calcInputField.value = "e";
+    else
+        calcInputField.value = parseFloat(calcInput);
+    //calcInputField.preventDefault();
 }
